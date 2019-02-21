70d11f644d220ceae955d187afd887ef1ede86c5
Hui Chen <>
2019-02-21 15:39:59 -0500

    added responsive/non-responsive example using Java threads (task & service)

---
+++ W4_Thread/benfits/responsiveness/ViaTasks/PiEstimatorFX/src/main/java/edu/cuny/brooklyn/design/PiEstimatorTask.java
@@ -0,0 +1,58 @@
+package edu.cuny.brooklyn.design;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.concurrent.Task;
+
+public class PiEstimatorTask extends Task<PiEstimatorState>{
+	private final static Logger LOGGER = LoggerFactory.getLogger(PiEstimatorTask.class);
+	
+	private final long numOfPoints;
+	private final Random rngX;
+	private final Random rngY;
+
+	public PiEstimatorTask(long numOfPoints, long seedX, long seedY) {
+		this.numOfPoints = numOfPoints;
+		this.rngX = new Random(seedX);
+		this.rngY = new Random(seedY);
+	}
+	
+	@Override
+	protected PiEstimatorState call() throws Exception {
+		long accepted = 0;
+		for (long i = 0; i < numOfPoints; i++) {
+			if (isCancelled()) {
+				break;
+			}
+			
+			double x = rngX.nextDouble();
+			double y = rngY.nextDouble();
+			boolean isAccepted = false;
+			if (x * x + y * y < 1.0) {
+				accepted++;
+				isAccepted = true;
+			} 
+			updateProgress(i, numOfPoints-1);
+			updateValue(new PiEstimatorState(x, y, isAccepted, accepted));
+			// LOGGER.debug("x = " + x + ", y = " + y);
+		}
+		return new PiEstimatorState(-1., -1., false, accepted);
+	}
+	
+	@Override
+	protected void succeeded() {
+		super.succeeded();
+		double pi = (double) getValue().getNumOfAccepted() / (double) numOfPoints * 4.0;
+		updateMessage(Double.toString(pi));
+		LOGGER.debug("completed running PI estimator in background ...");
+	}
+	
+	@Override
+	protected void cancelled() {
+		super.cancelled();
+		LOGGER.debug("Pi estimator task is canceled ...");
+	}
+}
