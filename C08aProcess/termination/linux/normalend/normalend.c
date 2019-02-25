01a430625eb5c976c971179253c1828b2293c398
Hui Chen <chen@sci.brooklyn.cuny.edu>
2019-02-25 15:08:52 -0500

    reorgnized examples

--- C08aProcess/termination/linux/normalend/normalend.c
+++ C08aProcess/termination/linux/normalend/normalend.c
@@ -1,18 +1,3 @@
-<<<<<<< HEAD:C08aProcess/termination/linux/normalend/normalend.c
-#include <stdio.h>
-
-/*
- * On Debian/Ubuntu:
- *
- * apt-get install strace
- *   make
- *   strace ./normal
- * */
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
-=======
 #include <stdio.h>
 
 /*
@@ -26,4 +11,3 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
->>>>>>> 19sp7310x/master:W3_Process/termination/linux/normal.c
