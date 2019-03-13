263edd632da40abc531cc0aa91999bb4229de512
huichen-cs <chen@sci.brooklyn.cuny.edu>
2019-03-13 11:32:48 -0400

    fixed a bug in contention scope example

--- C14aSchedule/pthread/contscope/contscopedemo.c
+++ C14aSchedule/pthread/contscope/contscopedemo.c
@@ -34,24 +34,30 @@ int main(int argc, char *argv[]) {
    /* set the scheduling algorithm to PCS or SCS */ 
    if (scope == PTHREAD_SCOPE_PROCESS) {
        printf("The default contention scope is PTHREAD_SCOPE_PROCESS\n"); 
-       if (change && pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
-           fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_SYSTEM\n");
-       } else {
-           printf("Successfully set scope as PTHREAD_SCOPE_SYSTEM\n");
+       if (change) {
+           if (pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
+               fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_SYSTEM\n");
+           } else {
+               printf("Successfully set scope as PTHREAD_SCOPE_SYSTEM\n");
+           }
        }
    } else if (scope == PTHREAD_SCOPE_SYSTEM) {
        printf("The default contention scope is PTHREAD_SCOPE_SYSTEM\n"); 
-       if (change && pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
-           fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_PROCESS\n");
-       } else {
-           printf("Successfully set scope as PTHREAD_SCOPE_PROCESS\n");
+       if (change) {
+           if (pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
+               fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_PROCESS\n");
+           } else {
+               printf("Successfully set scope as PTHREAD_SCOPE_PROCESS\n");
+           }
        }
    } else {
        fprintf(stderr, "Illegal scope value.\n"); 
-       if (change && pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
-           fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_SYSTEM\n");
-       } else {
-           printf("Successfully set scope as PTHREAD_SCOPE_SYSTEM\n");
+       if (change) {
+           if (pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) != 0) {
+               fprintf(stderr, "Failed to set scope as PTHREAD_SCOPE_SYSTEM\n");
+           } else {
+               printf("Successfully set scope as PTHREAD_SCOPE_SYSTEM\n");
+           }
        }
    }
 
