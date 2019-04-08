bdbe9ad98817bae890b1b6463a3faeccadcf716a
Hui Chen <chen@sci.brooklyn.cuny.edu>
2019-04-08 17:38:26 -0400

    updated race condition example

--- C21aSynchronization/race/pc/runtest.sh
+++ C21aSynchronization/race/pc/runtest.sh
@@ -6,9 +6,9 @@ while [ ${i} -lt ${nruns} ]; do
     result=`./producerconsumer | grep "The sum" | sed -e 's/^The sum is '//g`
     echo -n -e "\rThe result is ${result}"
     if [ ${result} -ne 49995000 ]; then
-        echo "The result ${result} is wrong!"
+        echo -e "\nThe result ${result} is wrong!"
         exit 1
     fi
     let i=${i}+1
 done
-echo "NO error detected"
+echo -e "\nNO error detected"
