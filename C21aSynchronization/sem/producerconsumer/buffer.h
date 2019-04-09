8879d13a7f77e681b0ac4c1c556af1a722dc2210
Hui Chen <chen@sci.brooklyn.cuny.edu>
2019-04-09 16:28:22 -0400

    added semaphore exxample

---
+++ C21aSynchronization/sem/producerconsumer/buffer.h
@@ -0,0 +1,14 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#define NUM_PRODUCED 10000
+#define BUFFER_SIZE 4096
+typedef struct sharedbuffer {
+    int buffer[BUFFER_SIZE];
+    int in;
+    int out;
+    int counter;
+    sem_t countersem;
+} sharedbuffer_t;
+
+#endif
