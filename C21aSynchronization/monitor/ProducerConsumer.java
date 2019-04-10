86f3be1048796df0c0b858e32651d33d992cd51d
Hui Chen <chen@sci.brooklyn.cuny.edu>
2019-04-10 10:27:52 -0400

    updated monitor example in Java (producer/consumer)

--- C21aSynchronization/monitor/ProducerConsumer.java
+++ C21aSynchronization/monitor/ProducerConsumer.java
@@ -1,66 +1,103 @@
 /**
- * This is the sample program in page 138,
+ * This is a minor revision of the sample program in page 138,
  *  Andrew S. Tanenbaum and Herbert Bos. 2014. Modern Operating Systems (4th
- *  ed.). Prentice Hall Press, Upper Saddle River, NJ, USA. 
+ *  ed.). Prentice Hall Press, Upper Saddle River, BUFFER_SIZEJ, USA. 
  */
-import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+/* import java.util.Random; */
 
 public class ProducerConsumer {
-    static final int N = 100;
-    static Producer p = new Producer();
-    static Consumer c = new Consumer();
+    static final int BUFFER_SIZE = 100;
+    static final int NUM_CONSUMED = 200;
+    static Consumer consumer = new Consumer();
+    static Producer producer = new Producer();
+    static Thread p = new Thread(producer);
+    static Thread c = new Thread(consumer);
     static ProducerConsumerMonitor monitor = new ProducerConsumerMonitor();
 
     public static void main(String[] args) {
         p.start();
         c.start();
+        try {
+            c.join();
+            producer.interrupt();
+            p.interrupt();
+            System.out.println("The sum is " + consumer.getSum());
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    static class Producer extends Thread {
+    static class Producer implements Runnable {
+        private AtomicBoolean running = new AtomicBoolean(false);
+        private int number;
+
+        public Producer() {
+            number = 0;
+        }
 
         @Override
         public void run() {
-            int item;
-            while (true) {
-                item = produceItem();
+            running.set(true);
+            while (running.get()) {
+                int item = produceItem();
                 monitor.insert(item);
             }
         }
 
         private int produceItem() {
+            /*
             Random rng = new Random();
             return rng.nextInt();
+            */
+            return number ++;
+        }
+
+
+        public void interrupt() {
+            running.set(false);
         }
     }
 
 
-    static class Consumer extends Thread {
+    static class Consumer implements Runnable {
+        private int sum; 
+
+        public Consumer() {
+            sum = 0;
+        }
 
         @Override
         public void run() {
-            int item;
-            while (true) {
-                item = monitor.remove();
+            for (int i=0; i<NUM_CONSUMED; i++) {
+                int item = monitor.remove();
                 consumeItem(item);
             }
         }
 
         private void consumeItem(int item) {
+            sum += item;
         }
+
+
+        public int getSum() {
+            return sum;
+        }
+
     }
 
     static class ProducerConsumerMonitor {
-        private int buffer[] = new int[N];
-        private int count = 0, low = 0, high = 0;
+        private int buffer[] = new int[BUFFER_SIZE];
+        private int count = 0, out = 0, in = 0;
 
         public synchronized void insert(int val) {
-            if (count == N) {      // buffer is full, sleep while waiting
+            if (count == BUFFER_SIZE) { // buffer is full, sleep while waiting
                 goToSleep();
             }
-            buffer[high] = val;    // insert the produced item to buffer
-            high = (high + 1) / N; // advance to next available slot
-            count = count + 1;     // count the number of items in buffer
-            if (count == 1) {      
+            buffer[in] = val;    // insert the produced item to buffer
+            in = (in + 1) % BUFFER_SIZE; // advance to next available slot
+            count ++;     // count the number of items in buffer
+            if (count > 0) {      
                 // buffer is not empty any more, wake up consumer to consume
                 notify();
             }
@@ -72,10 +109,10 @@ public class ProducerConsumer {
             if (count == 0) {
                 goToSleep();
             }
-            val = buffer[low];
-            low = (low + 1) % N;
-            count = count - 1;
-            if (count == N - 1) { 
+            val = buffer[out];
+            out = (out + 1) % BUFFER_SIZE;
+            count --;
+            if (count < BUFFER_SIZE) { 
                 // buffer is not full any more, wake up producer to produce
                 notify();
             }
