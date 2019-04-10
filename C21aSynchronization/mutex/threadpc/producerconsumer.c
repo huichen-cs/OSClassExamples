debdb3282cca17a34a3d0dceb150c5141a74f5c2
Hui Chen <chen@sci.brooklyn.cuny.edu>
2019-04-10 09:27:57 -0400

    added multithread producer/consumer using mutex

--- C21aSynchronization/mutex/threadpc/producerconsumer.c
+++ C21aSynchronization/mutex/threadpc/producerconsumer.c
@@ -1,5 +1,5 @@
 /**
- * This is the sample program in page 138,
+ * This is a revision of the sample program in page 138,
  *  Andrew S. Tanenbaum and Herbert Bos. 2014. Modern Operating Systems (4th
  *  ed.). Prentice Hall Press, Upper Saddle River, NJ, USA. 
  */
@@ -8,44 +8,71 @@
 #include <stdlib.h> 
 #include <pthread.h>
 
-#define MAX 1000
-pthread_mutex_t the_mutex;
-pthread_cond_t condc, condp;
-int buffer = 0;
+/* the bounded buffer */
+#define NUM_PRODUCED 10000
+#define BUFFER_SIZE 4096
+static int buffer[BUFFER_SIZE];
+static int in;
+static int out;
+static int counter;
+static pthread_mutex_t the_mutex;
+static pthread_cond_t condc, /* consumer waits */ 
+                      condp; /* producer waits */
 
 void *producer(void *ptr) {
     int i;
 
-    for (i=1; i<=MAX; i++) {
+    for (i=0; i<NUM_PRODUCED; i++) {
+        int nextproduced = i;
+
         pthread_mutex_lock(&the_mutex);
-        while (buffer != 0) {
-            pthread_cond_wait(&condp, &the_mutex);
+
+        while (counter == BUFFER_SIZE) {
+            /* the producer thread must wait when the buffer is full */
+            pthread_cond_wait(&condp, &the_mutex); 
         }
-        buffer = i;
-        pthread_cond_signal(&condc);
+
+        buffer[in] = nextproduced;
+        in = (in + 1) % BUFFER_SIZE;
+        counter ++;
+
+        pthread_cond_signal(&condc); /* wake up the consumer if it's sleeping */
         pthread_mutex_unlock(&the_mutex);
     }
     pthread_exit(0);
 }
 
 void *consumer(void *ptr) {
-    int i;
+    int i, nextconsumed, sum = 0;
 
-    for (i=1; i<=MAX; i++) {
+    for (i=0; i<NUM_PRODUCED; i++) {
+
         pthread_mutex_lock(&the_mutex);
-        while (buffer == 0) {
-            pthread_cond_wait(&condc, &the_mutex);
+
+        while (counter == 0) {
+            /* the consumer thread must wait when the buffer is empty */
+            pthread_cond_wait(&condc, &the_mutex); 
         }
-        buffer = 0;
-        pthread_cond_signal(&condp);
+
+        nextconsumed = buffer[out];
+        out = (out + 1) % BUFFER_SIZE;
+        counter --;
+
+        pthread_cond_signal(&condp); /* wake up the producer if it's sleeping */
         pthread_mutex_unlock(&the_mutex);
+        
+        /* consume the item */
+        sum += nextconsumed;
     }
+
+    printf("The sum is %d\n", sum);
     pthread_exit(0);
 }
 
 int main(int argc, char *argv[]) {
     pthread_t producer_thread, consumer_thread;
 
+    counter = 0; in = 0; out = 0;
     pthread_mutex_init(&the_mutex, 0);
     pthread_cond_init(&condc, 0);
     pthread_cond_init(&condp, 0);
---
+++ C21aSynchronization/mutex/threadpc/runtest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pprog=('-' '\' '|' '/' '-' '\' '|')
+nruns=10000
+i=1
+while [ ${i} -lt ${nruns} ]; do
+    resultline=`./producerconsumer | \
+        grep "The sum"` 
+    result=`echo ${resultline} | \
+        sed -rn 's/^The sum is ([0-9]+).*$/\1/p'`
+    let j=${i}%7
+    echo -n -e "\rThe result is ${result} ${pprog[${j}]} at run ${i}"
+    if [ x"${result}" != x"49995000" ]; then
+        echo -e \
+            " \nIs the result ${result} wrong? (or the output intermingled?)"
+        echo -e \
+            "\tThe line is\n\t${resultline}"
+    fi
+    let i=${i}+1
+done
+echo -e "\nCompleted!"
