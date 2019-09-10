2e25caf0ed49734cde9f3f8771c47a2feaf83f50
huichen-cs <chen@sci.brooklyn.cuny.edu>
2019-09-09 22:15:48 -0400

    disk load

--- bootsector/load/load2.asm
+++ bootsector/load/load2.asm
@@ -1,8 +1,87 @@
-mov cl, 0xae
+[org 0x7c00]
+; query disk drive parameters
+; 
+; load2.asm
+;
+; using PC BIOS's 0x13 interrupt
+; See
+;   https://en.wikipedia.org/wiki/INT_13H#INT_13h_AH=08h:_Read_Drive_Parameters
+
+READ_DISK:
+
+mov ah, 0x08
+mov dl, 0x80    ; 1st HDD
+mov ax, 0
+mov es, ax
+mov di, ax
+int 0x13
+
+jc .DISK_ERROR  ; using local label. local labels start from "."
+
+push cx
+
+push bx
+mov bx, DISK_NO
+call print_msg
+mov cl, dl
 call print_byte
-jmp $
+pop bx
+
+call print_line
+
+push bx
+mov bx, HEAD_IDX
+call print_msg
+mov cl, dh
+call print_byte
+pop bx
+
+call print_line
+
+pop cx
+
+push cx
+
+push bx
+mov bx, SECTOR_NUM
+call print_msg
+pop bx
+
+mov cl, ch
+call print_byte
+call print_space
+pop cx
+call print_byte
+
+pop cx
+
+jmp $    
+
+
+.DISK_ERROR:
+    mov bx, DISK_ERROR_MSG
+    call print_msg
+    
+    jmp $
+    
 
 %include "print_byte.asm"
+%include "print_msg.asm"
+%include "print_line.asm"
+%include "print_space.asm"
+
+DISK_ERROR_MSG:
+    db "Disk error", 0
+
+DISK_NO:
+    db "DISK # (hex): ", 0
+HEAD_IDX:
+    db "HEAD INDEX (hex): ", 0
+SECTOR_NUM:
+    db "CYL/SEC NUMBERS (hex): ", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+times 512 db 0
+times 512 db 0
--- bootsector/load/load3.asm
+++ bootsector/load/load3.asm
@@ -1,20 +1,59 @@
-mov ax, 0x7c0
-mov ds, ax
-mov bx, DATA
-mov cl, 64
-call print_mem
+[org 0x7c00]
+
+START:
+
+mov [BOOT_DRIVE], dl
+
+mov bp, 0x8000
+mov sp, bp
+
+mov ah, 0x00                ; resetting disk 
+int 0x13 
+
+
+; pass arguments
+mov ah, 0x02                ; int 13h function 02
+mov al, 0x02                ; read 2 sectors
+push ax
+
+mov ch, 0x00                ; cylinder no.
+mov cl, 0x02                ; sector no. (sector 1 is boot sector)
+push cx
+
+mov dh, 0x00                ; head no.
+mov dl, [BOOT_DRIVE]        ; drive no.
+push dx
+
+push es                     ; read disk data to es:bx
+mov bx, 0x9000
+push bx
+
+call read_disk              ; read disk data
+
+
+mov cl, [0x9000]
+call print_byte
+call print_space
+mov cl, [0x9000+512]
+call print_byte
 
 jmp $
 
-
-%include "print_mem.asm"
+%include "read_disk.asm"
 %include "print_byte.asm"
+%include "print_mem.asm"
+%include "print_msg.asm"
 %include "print_space.asm"
 %include "print_line.asm"
 
-DATA:
-times 8  db 0x36, 0x72, 0xae, 0xff, 0x3a, 0x11, 0x12, 0x13
 
-times 510-($-$$) db 0
+BOOT_DRIVE:
+    db 0
+
+times 510 -($-$$) db 0
 dw 0xaa55
+
+times 512 db 0xab
+times 512 db 0xba
+
 
