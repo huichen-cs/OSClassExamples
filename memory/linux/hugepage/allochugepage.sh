8599791f8bde9b73b952dc4d34d90bff3dd4df84
huichen-cs <chen@sci.brooklyn.cuny.edu>
2019-12-02 12:28:07 -0500

    for merging

--- memory/linux/hugepage/allochugepage.sh
+++ memory/linux/hugepage/allochugepage.sh
@@ -35,6 +35,10 @@ fi
 echo "The huge page allocation are as follows:"
 grep -i "Hugepage" /proc/meminfo
 
+apt-get install -y x86info libhugetlbfs0 > /dev/null 2>&1
+
+make
+
 time LD_PRELOAD=/usr/lib/i386-linux-gnu/libhugetlbfs-2.20.so HUGETLB_MORECORE=yes ./worker 4096 2
 
 time ./worker 4096 2
