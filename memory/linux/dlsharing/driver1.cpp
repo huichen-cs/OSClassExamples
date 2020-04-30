16081dc2ffadad3d17110b3b81c57bf769c5adc2
huichen-cs <chen@sci.brooklyn.cuny.edu>
2020-04-30 01:37:53 -0400

    added DLL example

---
+++ memory/linux/dlsharing/driver1.cpp
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <iostream>
+#include "vector.h"
+
+using namespace cisc3320;
+using namespace std;
+
+int main() {
+	RandomVector rv1(3);
+	RandomVector rv2(3);
+
+	double p = rv1.dotProduct(rv2);
+	
+	cout << "rv1=" << rv1 << endl;
+	cout << "rv2=" << rv2 << endl;
+	cout << "DotProduct(rv1,rv2)=" << p << endl;
+
+	sleep (360);
+	return 0;
+}
