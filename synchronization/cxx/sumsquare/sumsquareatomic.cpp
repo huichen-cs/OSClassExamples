6e5c777a62c0430743aac3b16e974ea6f4292bbb
huichen-cs <chen@sci.brooklyn.cuny.edu>
2020-04-30 20:30:46 -0400

    added cxx concurrency example

---
+++ synchronization/cxx/sumsquare/sumsquareatomic.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <atomic>
+#include <thread>
+#include <cstdlib>
+
+static std::atomic<int> sum(0);
+
+static void square(int n) {
+    sum += n * n;
+}
+
+int main(int argc, char *argv[]) {
+	std::thread *workers;
+	int nworkers = 10;
+
+	if (argc >= 2) {
+		nworkers = std::stoi(argv[1]);
+	}
+
+	workers = (std::thread *)malloc(sizeof(std::thread)*nworkers);
+	if (workers == NULL) {
+		std::cerr << "Memory allocation failed\n" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	/* Compute
+     * 1*1 + 2*2 + 3*3 + ... + (nworkers+1)*(nworkers+1)
+     * The expect result is 
+	 *      385 if nworkers is 10, 
+	 *     2870 if nworkers is 20
+     */
+    for (int i = 0; i < nworkers; i++) {
+        workers[i] = std::thread(&square, i+1);
+    }
+
+    for (int i=0; i<nworkers; i++) {
+        workers[i].join();
+    }
+
+    std::cout << "sum = " << sum << std::endl;
+    return 0;
+}
---
+++ synchronization/cxx/sumsquare/sumsquarefuture.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <future>
+#include <chrono>
+#include <cstdlib>
+
+static int square(int n) {
+	return n * n;
+}
+
+int main(int argc, char *argv[]) {
+	int nworkers = 10;
+
+	if (argc >= 2) {
+		nworkers = std::stoi(argv[1]);
+	}
+
+	/* Compute
+     * 1*1 + 2*2 + 3*3 + ... + (nworkers+1)*(nworkers+1)
+     * The expect result is 
+	 *      385 if nworkers is 10, 
+	 *     2870 if nworkers is 20
+     */
+	int sum = 0;
+    for (int i=0; i<nworkers; i++) {
+		std::future<int> s = std::async(&square, i+1);
+		sum += s.get();
+    }
+
+    std::cout << "sum = " << sum << std::endl;
+    return 0;
+}
