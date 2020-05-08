878cabead3daad897957cede0cd9cde0d267d704
huichen-cs <chen@sci.brooklyn.cuny.edu>
2020-05-08 13:35:34 -0400

    added cpuid tools

---
+++ memory/linux/hugepage/cachel2.c
@@ -0,0 +1,21 @@
+/*
+ * This is from
+ * https://en.wikipedia.org/wiki/CPUID#EAX=80000006h:_Extended_L2_Cache_Features
+ */
+#include <cpuid.h>  // GCC-provided
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void) {
+    uint32_t eax, ebx, ecx, edx;
+    if (__get_cpuid(0x80000006, &eax, &ebx, &ecx, &edx)) {
+        printf(
+            "Extended L2 Cache Features:\n" 
+            "\tLine size: %d B, Assoc. Type: %d; Cache Size: %d KB.\n", 
+            ecx & 0xff, (ecx >> 12) & 0x07, (ecx >> 16) & 0xffff);
+        return 0;
+    } else {
+        fprintf(stderr, "CPU does not support 0x80000006");
+        return 2;
+    }
+}
