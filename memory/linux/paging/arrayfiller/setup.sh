7288a529ed3233f8b817ecc10c0517588933589b
huichen-cs <chen@sci.brooklyn.cuny.edu>
2020-05-13 13:31:41 -0400

    arrayfiller improvement

--- memory/linux/paging/arrayfiller/setup.sh
+++ memory/linux/paging/arrayfiller/setup.sh
@@ -6,9 +6,20 @@ if [ ${uid} -ne 0 ]; then
     exit 1
 fi
 
+which cgexec > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo -n ">> Installing the cgroup-tools package ..."
+	apt-get install -y cgroup-tools iotop > /dev/null 2>&1
+	if [ $?  -ne 0 ]; then 
+		echo " Failed and quitting ..."
+		exit 1
+	else
+		echo " Succeeded."
+	fi
+fi
 
 if [ $# -lt 1 ]; then 
-   numofpages=64
+   numofpages=65
 else
    numofpages=$1
 fi
@@ -21,11 +32,17 @@ if [ -r /sys/fs/cgroup/memory/cisc3320arrayfilldemo ]; then
     cgdelete -g memory:/cisc3320arrayfilldemo
 fi
 cgcreate -g memory:/cisc3320arrayfilldemo
+if [ $? -ne 0 ]; then
+	echo "Failed to create the cgroup for arrayfilleri. Qutitting ..."
+	exit 1
+fi
 echo ${memlimit} > /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes
 
 echo ">> In /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes:"
 cat /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes
 echo ">> Physical memory limit set as ${memlimit} bytes or ${numofpages} pages."
+
+make > /dev/null 2>&1 
 
 if [ $# -lt 2 ] ; then
     echo ">> To run an array-filler program, issue the following as root:"
