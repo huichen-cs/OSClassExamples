3ba02f7eb55aef97dd9ef6c568dbac94dd686797
huichen-cs <chen@sci.brooklyn.cuny.edu>
2020-05-13 13:31:41 -0400

    arrayfiller improvement

--- memory/linux/paging/arrayfiller/showfaults.sh
+++ memory/linux/paging/arrayfiller/showfaults.sh
@@ -6,14 +6,21 @@ if [ $# -lt 1 ]; then
 fi
 
 pid=$1
-filecontent=`cat /proc/${pid}/stat`
-if [ $? -ne 0 ]; then
-    echo "Process (pid=${pid}) not found in the system"
-    exit 1
-fi
-minorfaults=`echo ${filecontent} | cut -d' ' -f10`
-majorfaults=`echo ${filecontent} | cut -d' ' -f12`
+while [ 1 ]; do
+		if [ ! -r /proc/${pid}/stat ]; then
+			echo "Process ${pid} terminated."
+			exit 0
+		fi
+		filecontent=$(cat /proc/${pid}/stat)
+		if [ $? -ne 0 ]; then
+			echo "Process (pid=${pid}) not found in the system"
+			exit 1
+		fi
+		minorfaults=$(echo ${filecontent} | cut -d' ' -f10)
+		majorfaults=$(echo ${filecontent} | cut -d' ' -f12)
 
-echo "Page Faults by Process (pid=${pid}): Minor ${minorfaults} Major ${majorfaults}"
+		echo "Page Faults by Process (pid=${pid}): Minor ${minorfaults} Major ${majorfaults}"
+		sleep 5
+done
 
 
