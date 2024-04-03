310b362ccb4ea86d4634ad07667f66f860aaa00a
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-04-03 12:19:15 -0400

    added README for race condition examples

--- synchronization/racecond/sumsquare/sumsquare.cpp
+++ synchronization/racecond/sumsquare/sumsquare.cpp
@@ -10,24 +10,24 @@ static void square(int n) {
 }
 
 int main(int argc, char *argv[]) {
-	std::thread *workers;
-	int nworkers = 10;
+    std::thread *workers;
+    int nworkers = 10;
 
-	if (argc >= 2) {
-		nworkers = std::stoi(argv[1]);
-	}
+    if (argc >= 2) {
+        nworkers = std::stoi(argv[1]);
+    }
 
-	workers = (std::thread *)malloc(sizeof(std::thread)*nworkers);
-	if (workers == NULL) {
-		std::cerr << "Memory allocation failed\n" << std::endl;
-		return EXIT_FAILURE;
-	}
+    workers = (std::thread *)malloc(sizeof(std::thread)*nworkers);
+    if (workers == NULL) {
+        std::cerr << "Memory allocation failed\n" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	/* Compute
+    /* Compute
      * 1*1 + 2*2 + 3*3 + ... + (nworkers+1)*(nworkers+1)
      * The expect result is 
-	 *      385 if nworkers is 10, 
-	 *     2870 if nworkers is 20
+     *      385 if nworkers is 10, 
+     *     2870 if nworkers is 20
      */
     for (int i = 0; i < nworkers; i++) {
         workers[i] = std::thread(&square, i+1);
