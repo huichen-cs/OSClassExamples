5899f101a3d50612021c195d5d0f0cfdafc34b17
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-05-06 21:43:30 +0000

    added error checking in setup.sh

--- memory/linux/paging/arrayfiller/setup.sh
+++ memory/linux/paging/arrayfiller/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cgname=$(mount | grep -E "[[:space:]]/sys/fs/cgroup[[:space:]]" | cut -d' ' -f1)
+if [ "${cgname}" != "cgroup" ]; then
+    echo "expected cgroup is mounted on /sys/fs/cgroup, but not found"
+    exit 1
+fi
+
 uid=`id -u`
 if [ ${uid} -ne 0 ]; then
     echo ">> This script needs to run as root."
@@ -28,29 +34,29 @@ pagesize=`getconf PAGESIZE`
 memlimit=$((${numofpages} * ${pagesize}))
 
 
-if [ -r /sys/fs/cgroup/memory/cisc3320arrayfilldemo ]; then
-    cgdelete -g memory:/cisc3320arrayfilldemo
+if [ -r /sys/fs/cgroup/memory/osarrayfilldemo ]; then
+    cgdelete -g memory:/osarrayfilldemo
 fi
-cgcreate -g memory:/cisc3320arrayfilldemo
+cgcreate -g memory:/osarrayfilldemo
 if [ $? -ne 0 ]; then
 	echo "Failed to create the cgroup for arrayfilleri. Qutitting ..."
 	exit 1
 fi
-echo ${memlimit} > /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes
+echo ${memlimit} > /sys/fs/cgroup/memory/osarrayfilldemo/memory.limit_in_bytes
 
-echo ">> In /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes:"
-cat /sys/fs/cgroup/memory/cisc3320arrayfilldemo/memory.limit_in_bytes
+echo ">> In /sys/fs/cgroup/memory/osarrayfilldemo/memory.limit_in_bytes:"
+cat /sys/fs/cgroup/memory/osarrayfilldemo/memory.limit_in_bytes
 echo ">> Physical memory limit set as ${memlimit} bytes or ${numofpages} pages."
 
 make > /dev/null 2>&1 
 
 if [ $# -lt 2 ] ; then
     echo ">> To run an array-filler program, issue the following as root:"
-    echo -e "\tcgexec -g memory:cisc3320arrayfilldemo ./arrayfillerbycolumn"
+    echo -e "\tcgexec -g memory:osarrayfilldemo ./arrayfillerbycolumn"
     echo -e "\t\tor"
-    echo -e "\tcgexec -g memory:cisc3320arrayfilldemo ./arrayfillerbyrow"
+    echo -e "\tcgexec -g memory:osarrayfilldemo ./arrayfillerbyrow"
     exit 0
 fi
 program=$2
 echo ">> Running ${program}"
-cgexec -g memory:cisc3320arrayfilldemo ./${program}
+cgexec -g memory:osarrayfilldemo ./${program}
