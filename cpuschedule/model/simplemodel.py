5098de5dd6053f39af9b3cdf76af72957acba174
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-10-16 21:57:47 +0000

    improved Python scripts for styling

--- cpuschedule/model/simplemodel.py
+++ cpuschedule/model/simplemodel.py
@@ -1,58 +1,80 @@
-import numpy as np
+"""Simple model for CPU utlization"""
+
 import os
+
+import numpy as np
 from matplotlib import pyplot as plt
 
+
 def compute_cpu_utilization(io_waits, process_numbers):
-    utilization = [];
+    utilization = []
     for w in io_waits:
         utilization.append(1 - w**process_numbers)
     return np.array(utilization)
 
-def setup_font_sizes():
-    SMALL_SIZE = 14
-    MEDIUM_SIZE = 16
-    BIG_SIZE = 20
-    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
 
-    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-    plt.rc('figure', titlesize=BIG_SIZE)     # fontsize of the figure title
+def setup_font_sizes():
+    small_size = 14
+    medium_size = 16
+    big_size = 20
+    plt.rc("font", size=small_size)  # controls default text sizes
+    plt.rc("axes", titlesize=small_size)  # fontsize of the axes title
+    plt.rc("axes", labelsize=medium_size)  # fontsize of the x and y labels
+
+    plt.rc("xtick", labelsize=small_size)  # fontsize of the tick labels
+    plt.rc("ytick", labelsize=small_size)  # fontsize of the tick labels
+    plt.rc("legend", fontsize=small_size)  # legend fontsize
+    plt.rc("figure", titlesize=big_size)  # fontsize of the figure title
+
 
 def save_graph(graph_fn):
-    figure_dir = 'figures'
-    try:
-        os.makedirs(figure_dir)
-    except:
-        pass
-    plt.savefig(os.path.join(figure_dir, graph_fn + '.pdf'))
-    plt.savefig(os.path.join(figure_dir, graph_fn + '.pdf'))
-    
+    figure_dir = "figures"
+    os.makedirs(figure_dir, exist_ok=True)
+    plt.savefig(os.path.join(figure_dir, graph_fn + ".pdf"))
+    plt.savefig(os.path.join(figure_dir, graph_fn + ".pdf"))
+
+
 def plot_graph(utilization, io_waits, process_numbers, graph_fn):
     setup_font_sizes()
     _, ax = plt.subplots(figsize=(8, 6))
-    linestyles = ['-', '--', '-.']
+    linestyles = ["-", "--", "-."]
     linewidths = [1, 1, 1]
-    colors = ['k', 'k', 'k']
-    markers = ['o', '*', '.']
+    colors = ["k", "k", "k"]
+    markers = ["o", "*", "."]
     markersizes = [6, 8, 6]
-    markerfacecolors = ['none', 'none', 'k']
+    markerfacecolors = ["none", "none", "k"]
     legends = []
-    for io_wait,row,style,color,width,marker,size,facecolor in zip(io_waits, utilization, linestyles, colors, linewidths, markers, markersizes, markerfacecolors):
-        ax.plot(process_numbers, row, linestyle=style, color=color, linewidth=width, marker=marker, markersize=size, markerfacecolor=facecolor)
-        legends.append('I/O Wait={0:2d}%'.format(int(io_wait*100)))
+    for io_wait, row, style, color, width, marker, size, facecolor in zip(
+        io_waits,
+        utilization,
+        linestyles,
+        colors,
+        linewidths,
+        markers,
+        markersizes,
+        markerfacecolors,
+    ):
+        ax.plot(
+            process_numbers,
+            row,
+            linestyle=style,
+            color=color,
+            linewidth=width,
+            marker=marker,
+            markersize=size,
+            markerfacecolor=facecolor,
+        )
+        legends.append(f"I/O Wait={int(io_wait * 100):2d}%")
     ax.legend(legends)
-    ax.set_xlabel('Degree of Multiprogramming')
-    ax.set_ylabel('CPU Utilization')
+    ax.set_xlabel("Degree of Multiprogramming")
+    ax.set_ylabel("CPU Utilization")
     plt.tight_layout()
     save_graph(graph_fn)
     plt.show()
 
-if __name__ == '__main__':
-    io_waits = np.array([0.2, 0.5, 0.8])
-    process_numbers = np.array(range(0, 13))
-    utilization = compute_cpu_utilization(io_waits, process_numbers)
-    plot_graph(utilization, io_waits, process_numbers, 'multiprogramming')
-    
+
+if __name__ == "__main__":
+    io_wait_ratios = np.array([0.2, 0.5, 0.8])
+    num_process_range = np.array(range(0, 13))
+    cpu_utilization = compute_cpu_utilization(io_wait_ratios, num_process_range)
+    plot_graph(cpu_utilization, io_wait_ratios, num_process_range, "multiprogramming")
--- process/model/multiprogrammodelsim.py
+++ process/model/multiprogrammodelsim.py
@@ -1,8 +1,9 @@
+"""Simple multiprogramming model"""
+
 import numpy as np
-
 from matplotlib import pyplot as plt
+from simplemodel import compute_cpu_utilization, save_graph, setup_font_sizes
 
-from simplemodel import setup_font_sizes, save_graph, compute_cpu_utilization
 
 def simulate_multiprogramming(io_wait, process_number, max_scheduling_quantums):
     busy_quantums = 0
@@ -19,63 +20,97 @@ def simulate_multiprogramming(io_wait, process_number, max_scheduling_quantums):
     utilization = busy_quantums / max_scheduling_quantums
     return utilization
 
+
 def setup_graph():
     setup_font_sizes()
     _, ax = plt.subplots(figsize=(8, 6))
     return ax
 
+
 def add_labels_and_legends(ax, legends):
     ax.legend(legends)
-    ax.set_xlabel('Degree of Multiprogramming')
-    ax.set_ylabel('CPU Utilization')
+    ax.set_xlabel("Degree of Multiprogramming")
+    ax.set_ylabel("CPU Utilization")
+
 
 def showsave_graph(graph_fn):
     plt.tight_layout()
     save_graph(graph_fn)
     plt.show()
 
+
 def plot_simulation_points(ax, utilization, io_waits, process_numbers):
-    linestyles = ['none', 'none', 'none']
+    linestyles = ["none", "none", "none"]
     linewidths = [1, 1, 1]
-    colors = ['k', 'k', 'k']
-    markers = ['o', '*', '.']
+    colors = ["k", "k", "k"]
+    markers = ["o", "*", "."]
     markersizes = [6, 8, 6]
-    markerfacecolors = ['none', 'none', 'k']
+    markerfacecolors = ["none", "none", "k"]
     legends = []
-    for io_wait,row,style,color,width,marker,size,facecolor in zip(io_waits, utilization, linestyles, colors, linewidths, markers, markersizes, markerfacecolors):
-        ax.plot(process_numbers, row, linestyle=style, color=color, linewidth=width, marker=marker, markersize=size, markerfacecolor=facecolor)
-        legends.append('Simulation:I/O Wait={0:2d}%'.format(int(io_wait*100)))
+    for io_wait, row, style, color, width, marker, size, facecolor in zip(
+        io_waits,
+        utilization,
+        linestyles,
+        colors,
+        linewidths,
+        markers,
+        markersizes,
+        markerfacecolors,
+    ):
+        ax.plot(
+            process_numbers,
+            row,
+            linestyle=style,
+            color=color,
+            linewidth=width,
+            marker=marker,
+            markersize=size,
+            markerfacecolor=facecolor,
+        )
+        legends.append(f"Simulation:I/O Wait={int(io_wait * 100):2d}%")
     return legends
 
+
 def plot_analytics_lines(ax, utilization, io_waits, process_numbers):
-    linestyles = ['-', '--', '-.']
+    linestyles = ["-", "--", "-."]
     linewidths = [1, 1, 1]
-    colors = ['k', 'k', 'k']
+    colors = ["k", "k", "k"]
     legends = []
-    for io_wait,row,style,color,width in zip(io_waits, utilization, linestyles, colors, linewidths):
+    for io_wait, row, style, color, width in zip(
+        io_waits, utilization, linestyles, colors, linewidths
+    ):
         ax.plot(process_numbers, row, linestyle=style, color=color, linewidth=width)
-        legends.append('Analytic:I/O Wait={0:2d}%'.format(int(io_wait*100)))
+        legends.append(f"Analytic:I/O Wait={int(io_wait * 100):2d}%")
     return legends
+
 
 def plot_graph(utilizations_sim, utilizations_ana, io_waits, process_numbers, graph_fn):
     ax = setup_graph()
-    legends_sim = plot_simulation_points(ax, utilizations_sim, io_waits, process_numbers)
+    legends_sim = plot_simulation_points(
+        ax, utilizations_sim, io_waits, process_numbers
+    )
     legends_ana = plot_analytics_lines(ax, utilizations_ana, io_waits, process_numbers)
     legends = legends_sim + legends_ana
     add_labels_and_legends(ax, legends)
     showsave_graph(graph_fn)
-    
 
-if __name__ == '__main__':
-    io_waits = np.array([0.2, 0.5, 0.8])
-    process_numbers = np.array(range(0, 13))
-    max_scheduling_quantums = 1000
-    utilizations_sim = []
-    for io_wait in io_waits:
+
+if __name__ == "__main__":
+    io_wait_list = np.array([0.2, 0.5, 0.8])
+    num_process_range = np.array(range(0, 13))
+    max_quantum = 1000
+    util_sim = []
+    for io_w in io_wait_list:
         u_for_io_wait = []
-        for process_number in process_numbers:
-            u = simulate_multiprogramming(io_wait, process_number, max_scheduling_quantums)
+        for processes in num_process_range:
+            u = simulate_multiprogramming(io_w, processes, max_quantum)
             u_for_io_wait.append(u)
-        utilizations_sim.append(np.array(u_for_io_wait))
-    utilizations_ana = compute_cpu_utilization(io_waits, process_numbers)
-    plot_graph(utilizations_sim, utilizations_ana, io_waits, process_numbers, 'multiprogramming_sim')
+        util_sim.append(np.array(u_for_io_wait))
+    util_ana = compute_cpu_utilization(io_wait_list, num_process_range)
+    plot_graph(
+        util_sim,
+        util_ana,
+        io_wait_list,
+        num_process_range,
+        "multiprogramming_sim",
+    )
